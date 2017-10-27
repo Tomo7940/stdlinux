aafbf5c11961e9a7ed9f48484c0b7d08f0cc0b70
stz0206 <stz0206@linecorp.com>
2017-10-27 10:18:32 +0900

    Impl httpd2

--- httpd_advanced.c
+++ httpd_advanced.c
@@ -11,6 +11,8 @@
 #include <stdarg.h>
 #include <ctype.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 /***** Constants ******************************************/
 #define SERVER_NAME "LittleHTTP"
@@ -20,6 +22,9 @@
 #define LINE_BUF_SIZE 4096
 #define MAX_REQUEST_BODY_LENGTH (1024 * 1024)
 #define TIME_BUF_SIZE 64
+#define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=g] <docroot>\n"
+#define MAX_BACKLOG 5
+#define DEFAULT_PORT "80"
 
 /***** Data Type Definitions ******************************/
 struct HTTPHeaderField {
@@ -42,6 +47,16 @@ struct FileInfo {
     long size;
     int ok;
 };
+
+static struct option longopts[] = {
+    {"debug",   no_argument,        &debug_mode, 1},
+    {"chroot",  no_argument,        NULL,       'c'},
+    {"user",    required_argument,  NULL,       'u'},
+    {"group",   required_argument,  NULL,       'g'},
+    {"port",    required_argument,  NULL,       'p'},
+    {"help",    no_argument,        NULL,       'h'},
+    {0, 0, 0, 0}
+}
 
 /***** Function Prototypes ********************************/
 typedef void (*sighandler_t)(int);
@@ -68,18 +83,64 @@ static void free_fileinfo(struct FileInfo *info);
 static char* guess_content_type(struct FileInfo *info);
 static void* xmalloc(size_t sz);
 static void log_exit(char *fmt, ...);
+static int listen_socket(char *port);
 
 /***** Functions ******************************************/
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+    int server_fd;
+    char *port = NULL;
+    char *docroot;
+    int do_chroot = 0;
+    char *user = NULL;
+    char *group = NULL;
+    int opt;
+
+    while ((opt = getopt_long(argc, argv, "", longopts, NULL)) != 1) {
+        switch (opt) {
+            case 0:
+                break;
+            case 'c':
+                do_chroot = optarg;
+                break;
+            case 'u':
+                user = optarg;
+                break;
+            case 'g':
+                group = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case 'h':
+                fprintf(stdout, USAGE, argv[0]);
+                exit(0);
+            case '?':
+                fprintf(stdout, USAGE, argv[0]);
+                exit(1);
+        }
+    }
+
+    if (optind != argc -1) {
+        fprintf(stderr, USAGE, argv[0]);
         exit(1);
     }
 
+    docroot = argv[optind];
+
+    if (do_chroot) {
+        setup_environment(docroot, user, group);
+        docroot = "";
+    }
+
     install_signal_handlers();
-    service(stdin, stdout, argv[1]);
+    server_fd = listen_socket(port);
+    if (!debug_mode) {
+        openlog(SERVER_NAME, LOG_PID|LOG_NDELAY, LOG_DAEMON);
+        become_daemon();
+    }
+    server_main(server_fd, docroot);
     exit(0);
 }
 
@@ -445,4 +506,38 @@ static char*
 guess_content_type(struct FileInfo *info)
 {
     return "text/plain";
+}
+
+static int
+listen_socket(char *port)
+{
+    struct addrinfo hints, *res, *ai;
+    int err;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0) {
+        log_exit(gai_strerror(err));
+    }
+
+    for (ai = res; ai; ai = ai->ai_next) {
+        int sock;
+
+        sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+        if (sock < 0) continue;
+        if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+            close(sock);
+            continue;
+        }
+        if (listen(sock, MAX_BACKLOG) < 0) {
+            close(sock);
+            continue;
+        }
+        freeaddrinfo(res);
+        return sock;
+    }
+    log_exit("failed to listen socket");
+    return -1;
 }
